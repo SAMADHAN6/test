275fa3325117b905c2fb3dd0fb763fe1923e8a89
SAMADHAN6 <ravanpatil34@gmail.com>
2025-02-01 22:45:57 +0530

    added 1st timr file on git

--- pizza_finalprice.py
+++ pizza_finalprice.py
@@ -1,34 +1,17 @@
-#target 3: build an automatic pizza order program.
+wel="welcome to py pizza"
+print(wel.center(40,'*'))
+a,ex=(input('which pizza you want')),int(input("extra peporini"))
+if a=='small' :
+    c=15+2
+    print(15+2)
+elif a=='medium':
+    print(20+3)
+    c=20+3
 
-print("Hello! Welcome to Python Pizza Deliveries!")
-print("**********************************************")
-size=input("What size pizza do you want?S,M or L: ")
-if (size.upper() == "S"):
-	bill=15;
-elif (size.upper() == "M"):
-	bill=20
-elif (size.upper() == "L"):
-	bill=25
 else:
-	print("Wrong input")
-pepperoni=input("Do you want pepperoni on your pizza? Y or N: ")
-if( pepperoni.upper() == "Y" and (size.upper()=="M" or size.upper() == "L")):
-	bill+=3
-elif (pepperoni.upper() == "Y" and size.upper()=="S"):
-	bill+=2
-elif (pepperoni.upper()=="N"):
-	pass
-else:
-	print("Wrong Input!")
+   c=15+3
+   print(15+3)
 
-extra_cheese=input("Do you want extra cheese? Y or N: ")
-if(extra_cheese.upper() == "Y"):
-	bill+=1
-elif (extra_cheese.upper()=="N"):
-	pass
-else:
-	print("Wrong Input")
 
-print("**********************************************")
-print(f"Your final bill is: ${bill} ")
+print(f'{int(c)+ex} is your total bill ')
 
